1323d76fc62269a141606f8541713c3ecdefdb01
9hafidz6 <9hafidz6@gmail.com>
2019-10-17 23:17:36 +0800

    readded Dishes and DishList classes

---
+++ src/main/java/duke/recipebook/Dishes.java
@@ -0,0 +1,51 @@
+package duke.recipebook;
+
+import java.util.List;
+
+public class Dishes{
+
+    private String dishname;
+    private int total;
+    private float rating;
+    private List<String> ingredientsList = null;
+
+    public Dishes(String name) {
+        this.dishname = name;
+    }
+
+    public Dishes() {
+        //
+    }
+
+    public int getTotalNumberOfOrders() {
+        return total;
+    }
+
+    public void setNumberOfOrders(int amount) {
+        total = total + amount;
+    }
+
+    public void clearOrders() {
+        total = 0;
+    }
+
+    public String getDishname() {
+        return dishname;
+    }
+
+    public void setRating(int r) {
+        rating = (total * rating + r) / total;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void addIngredients(String ingredients) {
+        ingredientsList.add(ingredients);
+    }
+
+    public String toString() {
+        return dishname;
+    }
+}
